58ea564857832d797107d24b1bb5ec8c44ec5fd4
nik-se0 <74305462+nik-se0@users.noreply.github.com>
2023-04-29 07:54:46 +0300

    add node

--- modules/binary tree/include/binary_tree.h
+++ modules/binary tree/include/binary_tree.h
@@ -3,6 +3,14 @@
 #ifndef MODULES_BINARY_TREE_INCLUDE_BINARY_TREE_H_
 #define MODULES_BINARY_TREE_INCLUDE_BINARY_TREE_H_
 
-// file
+struct Node {
+    int Value;
+    Node* Left;
+    Node* Right;
+    Node* Parent;
+
+    Node(const int V = 0.0, Node* P = nullptr, Node* L = nullptr, Node* R = nullptr);
+    Node(const Node& n);
+};
 
 #endif  // MODULES_BINARY_TREE_INCLUDE_BINARY_TREE_H_
--- modules/binary tree/src/binary_tree.cpp
+++ modules/binary tree/src/binary_tree.cpp
@@ -1,3 +1,8 @@
 // Copyright 2023 Semenova Veronika
 
-// file
+#include <gtest/gtest.h>
+
+#include "include/binary_tree.h"
+
+Node::Node(const int V, Node* P, Node* L, Node* R) : Value(V), Left(L), Right(R), Parent(P) {}
+Node::Node(const Node& n) : Value(n.Value), Left(nullptr), Right(nullptr), Parent(nullptr) {}
