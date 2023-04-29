990242ea664715288d9daab32ef4d2c8b709a1cf
nik-se0 <74305462+nik-se0@users.noreply.github.com>
2023-04-29 19:24:37 +0300

    create app

---
+++ modules/priority-queue/include/priority_queue_application.h
@@ -0,0 +1,11 @@
+// Copyright 2023 Semenova Veronika
+
+#ifndef MODULES_PRIORITY_QUEUE_INCLUDE_PRIORITY_QUEUE_H_
+#define MODULES_PRIORITY_QUEUE_INCLUDE_PRIORITY_QUEUE_H_
+
+class PriorityQueueApp {
+ public:
+  PriorityQueueApp();
+};
+
+#endif  // MODULES_PRIORITY_QUEUE_INCLUDE_PRIORITY_QUEUE_H_
---
+++ modules/priority-queue/src/priority_queue_application.cpp
@@ -0,0 +1,6 @@
+// Copyright 2023 Semenova Veronika
+
+#include "include/priority_queue_application.h"
+#include "include/priority_queue.h"
+
+PriorityQueueApp::PriorityQueueApp() {};
