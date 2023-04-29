e50868779a666711607399902523393a1d698a2a
nik-se0 <74305462+nik-se0@users.noreply.github.com>
2023-04-29 09:33:33 +0300

    add node

--- modules/binary-tree/src/binary_tree.cpp
+++ modules/binary-tree/src/binary_tree.cpp
@@ -3,3 +3,10 @@
 #include <gtest/gtest.h>
 
 #include "include/binary_tree.h"
+
+Node::Node(const int V, Node* P, 
+ Node* L, Node* R) : Value(V), Left(L), 
+ Right(R), Parent(P) {}
+Node::Node(const Node& n) : 
+ Value(n.Value), Left(nullptr), 
+ Right(nullptr), Parent(nullptr) {}
--- modules/binary-tree/test/test_binary_tree.cpp
+++ modules/binary-tree/test/test_binary_tree.cpp
@@ -1,10 +1,74 @@
-//  Copyright 2023 Semenova Veronika
+// Copyright 2023 Semenova Veronika
 
 #include <gtest/gtest.h>
 
 #include "include/binary_tree.h"
 
-TEST(Semenova_Veronika_Node, Can_create_Node)
-{
+TEST(Semenova_Veronika_Node, Can_create_Node){
  ASSERT_NO_THROW(Node ed);
+}
+TEST(Semenova_Veronika_Node,
+ Can_create_Node_with_Value){
+ ASSERT_NO_THROW(Node ed(3));
+}
+TEST(Semenova_Veronika_Node,
+ Create_Node_with_Value_correctly){
+ Node ed(3);
+ EXPECT_EQ(ed.Value, 3);
+}
+TEST(Semenova_Veronika_Node,
+ Replace_value_Node_correctly){
+ Node ed(3);
+ ed.Value = 1;
+ EXPECT_EQ(ed.Value, 1);
+}
+TEST(Semenova_Veronika_Node,
+ Can_create_Node_with_parent){
+ Node ed1;
+ ASSERT_NO_THROW(Node ed(3, &ed1));
+}
+TEST(Semenova_Veronika_Node,
+ Create_Node_with_parent_correctly){
+ Node ed1(1);
+ Node ed(3, &ed1);
+
+ EXPECT_EQ((ed.Parent)->Value, 1);
+}
+TEST(Semenova_Veronika_Node,
+ Can_create_Node_with_neighbor){
+ Node ed1;
+ Node ed2(2);
+ Node ed3(3);
+ ASSERT_NO_THROW(Node ed(3,&ed1,&ed2,&ed3));
+}
+TEST(Semenova_Veronika_Node,
+ Create_Node_with_neighbor_correctly){
+ Node ed1(1);
+ Node ed2(2);
+ Node ed3(3);
+ Node ed(3, &ed1, &ed2, &ed3);
+
+ EXPECT_EQ((ed.Left)->Value, 2);
+ EXPECT_EQ((ed.Right)->Value, 3);
+
+}
+TEST(Semenova_Veronika_Node,
+ Can_create_Node_with_copy_constructor){
+ Node ed1;
+ ASSERT_NO_THROW(Node ed(ed1));
+
+}
+TEST(Semenova_Veronika_Node,
+ Create_Node_with_copy_constructor_correctly){
+
+ Node ed1(1);
+ Node ed2(2);
+ Node ed3(3, &ed1, &ed2);
+
+ Node e(ed3);
+
+ EXPECT_EQ(e.Value, 3);
+ EXPECT_EQ(e.Left, nullptr);
+ EXPECT_EQ(e.Right, nullptr);
+ EXPECT_EQ(e.Parent, nullptr);
 }
