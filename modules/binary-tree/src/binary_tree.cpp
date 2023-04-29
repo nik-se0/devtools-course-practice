bb5a267e6a5cf797a758b4cae76d7bd7cd763030
nik-se0 <74305462+nik-se0@users.noreply.github.com>
2023-04-29 09:59:32 +0300

    node fix style2

--- modules/binary-tree/src/binary_tree.cpp
+++ modules/binary-tree/src/binary_tree.cpp
@@ -5,8 +5,8 @@
 #include "include/binary_tree.h"
 
 Node::Node(const int V, Node* P,
-	Node* L, Node* R) : Value(V), Left(L),
-	Right(R), Parent(P) {}
+  Node* L, Node* R) : Value(V), Left(L),
+  Right(R), Parent(P) {}
 Node::Node(const Node& n) :
-	Value(n.Value), Left(nullptr),
-	Right(nullptr), Parent(nullptr) {}
+  Value(n.Value), Left(nullptr),
+  Right(nullptr), Parent(nullptr) {}
