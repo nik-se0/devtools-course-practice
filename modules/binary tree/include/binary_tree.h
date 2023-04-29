f7c730faac6c9fed4270b2d2e36fb70f4f6c6084
nik-se0 <74305462+nik-se0@users.noreply.github.com>
2023-04-29 08:56:56 +0300

    Delete binary_tree.h

--- modules/binary tree/include/binary_tree.h
+++
@@ -1,8 +0,0 @@
-// Copyright 2023 Semenova Veronika
-
-#ifndef MODULES_BINARY_TREE_INCLUDE_BINARY_TREE_H_
-#define MODULES_BINARY_TREE_INCLUDE_BINARY_TREE_H_
-
-struct Node {};
-
-#endif // MODULES_BINARY_TREE_INCLUDE_BINARY_TREE_H_
