10e61d011650872f1e007dca934da31ac2969930
nik-se0 <74305462+nik-se0@users.noreply.github.com>
2023-04-29 09:44:22 +0300

    node fix style

--- modules/binary-tree/include/binary_tree.h
+++ modules/binary-tree/include/binary_tree.h
@@ -4,14 +4,14 @@
 #define MODULES_BINARY_TREE_INCLUDE_BINARY_TREE_H_
 
 struct Node {
- int Value;
- Node* Left;
- Node* Right;
- Node* Parent;
+  int Value;
+  Node* Left;
+  Node* Right;
+  Node* Parent;
 
- Node(const int V = 0.0, Node* P = nullptr,
- Node* L = nullptr, Node* R = nullptr);
- explicit Node(const Node& n);
+  Node(const int V = 0.0, Node* P = nullptr,
+  Node* L = nullptr, Node* R = nullptr);
+  explicit Node(const Node& n);
 };
 
 #endif  //  MODULES_BINARY_TREE_INCLUDE_BINARY_TREE_H_
--- modules/binary-tree/test/test_binary_tree.cpp
+++ modules/binary-tree/test/test_binary_tree.cpp
@@ -4,71 +4,71 @@
 
 #include "include/binary_tree.h"
 
-TEST(Semenova_Veronika_Node, Can_create_Node){
- ASSERT_NO_THROW(Node ed);
+TEST(Semenova_Veronika_Node, Can_create_Node) {
+	ASSERT_NO_THROW(Node ed);
 }
 TEST(Semenova_Veronika_Node,
- Can_create_Node_with_Value){
- ASSERT_NO_THROW(Node ed(3));
+	Can_create_Node_with_Value) {
+	ASSERT_NO_THROW(Node ed(3));
 }
 TEST(Semenova_Veronika_Node,
- Create_Node_with_Value_correctly){
- Node ed(3);
- EXPECT_EQ(ed.Value, 3);
+	Create_Node_with_Value_correctly) {
+	Node ed(3);
+	EXPECT_EQ(ed.Value, 3);
 }
 TEST(Semenova_Veronika_Node,
- Replace_value_Node_correctly){
- Node ed(3);
- ed.Value = 1;
- EXPECT_EQ(ed.Value, 1);
+	Replace_value_Node_correctly) {
+	Node ed(3);
+	ed.Value = 1;
+	EXPECT_EQ(ed.Value, 1);
 }
 TEST(Semenova_Veronika_Node,
- Can_create_Node_with_parent){
- Node ed1;
- ASSERT_NO_THROW(Node ed(3, &ed1));
+	Can_create_Node_with_parent) {
+	Node ed1;
+	ASSERT_NO_THROW(Node ed(3, &ed1));
 }
 TEST(Semenova_Veronika_Node,
- Create_Node_with_parent_correctly){
- Node ed1(1);
- Node ed(3, &ed1);
+	Create_Node_with_parent_correctly) {
+	Node ed1(1);
+	Node ed(3, &ed1);
 
- EXPECT_EQ((ed.Parent)->Value, 1);
+	EXPECT_EQ((ed.Parent)->Value, 1);
 }
 TEST(Semenova_Veronika_Node,
- Can_create_Node_with_neighbor){
- Node ed1;
- Node ed2(2);
- Node ed3(3);
- ASSERT_NO_THROW(Node ed(3,&ed1,&ed2,&ed3));
+	Can_create_Node_with_neighbor) {
+	Node ed1;
+	Node ed2(2);
+	Node ed3(3);
+	ASSERT_NO_THROW(Node ed(3, &ed1, &ed2, &ed3));
 }
 TEST(Semenova_Veronika_Node,
- Create_Node_with_neighbor_correctly){
- Node ed1(1);
- Node ed2(2);
- Node ed3(3);
- Node ed(3, &ed1, &ed2, &ed3);
+	Create_Node_with_neighbor_correctly) {
+	Node ed1(1);
+	Node ed2(2);
+	Node ed3(3);
+	Node ed(3, &ed1, &ed2, &ed3);
 
- EXPECT_EQ((ed.Left)->Value, 2);
- EXPECT_EQ((ed.Right)->Value, 3);
-
-}
-TEST(Semenova_Veronika_Node,
- Can_create_Node_with_copy_constructor){
- Node ed1;
- ASSERT_NO_THROW(Node ed(ed1));
+	EXPECT_EQ((ed.Left)->Value, 2);
+	EXPECT_EQ((ed.Right)->Value, 3);
 
 }
 TEST(Semenova_Veronika_Node,
- Create_Node_with_copy_constructor_correctly){
+	Can_create_Node_with_copy_constructor) {
+	Node ed1;
+	ASSERT_NO_THROW(Node ed(ed1));
 
- Node ed1(1);
- Node ed2(2);
- Node ed3(3, &ed1, &ed2);
+}
+TEST(Semenova_Veronika_Node,
+	Create_Node_with_copy_constructor_correctly) {
 
- Node e(ed3);
+	Node ed1(1);
+	Node ed2(2);
+	Node ed3(3, &ed1, &ed2);
 
- EXPECT_EQ(e.Value, 3);
- EXPECT_EQ(e.Left, nullptr);
- EXPECT_EQ(e.Right, nullptr);
- EXPECT_EQ(e.Parent, nullptr);
+	Node e(ed3);
+
+	EXPECT_EQ(e.Value, 3);
+	EXPECT_EQ(e.Left, nullptr);
+	EXPECT_EQ(e.Right, nullptr);
+	EXPECT_EQ(e.Parent, nullptr);
 }
