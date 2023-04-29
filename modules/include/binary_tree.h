1312a9f496c24516577e10b9564cab7f2ed19185
nik-se0 <74305462+nik-se0@users.noreply.github.com>
2023-04-29 09:02:10 +0300

    add one

---
+++ modules/include/binary_tree.h
@@ -0,0 +1,8 @@
+//  Copyright 2023 Semenova Veronika
+
+#ifndef MODULES_BINARY_TREE_INCLUDE_BINARY_TREE_H_
+#define MODULES_BINARY_TREE_INCLUDE_BINARY_TREE_H_
+
+struct Node {};
+
+#endif //  MODULES_BINARY_TREE_INCLUDE_BINARY_TREE_H_
---
+++ modules/test/test_binary_tree.cpp
@@ -0,0 +1,10 @@
+//  Copyright 2023 Semenova Veronika
+
+#include <gtest/gtest.h>
+
+#include "include/binary_tree.h"
+
+TEST(Semenova_Veronika_Node, Can_create_Node)
+{
+ ASSERT_NO_THROW(Node ed);
+}
